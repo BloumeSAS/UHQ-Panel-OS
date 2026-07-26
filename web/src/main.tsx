bb4d575ae522ed1cd152bb58acafc08fbcb8d354
Mr X <93528905+JulesZYTB@users.noreply.github.com>
2026-07-26 02:32:55 +0200

    feat(theme,ui): wire ThemeProvider/DensityProvider providers

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { I18nProvider } from '@/lib/i18n';
 import { ThemeProvider } from '@/lib/theme';
+import { DensityProvider } from '@/lib/density';
 import { AuthProvider } from '@/lib/auth';
 import { SiteProvider } from '@/lib/site';
 import { Toaster } from '@/components/Toaster';
@@ -17,14 +18,16 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <I18nProvider>
-        <ThemeProvider>
-          <SiteProvider>
-            <AuthProvider>
-              <App />
-              <Toaster />
-            </AuthProvider>
-          </SiteProvider>
-        </ThemeProvider>
+        <SiteProvider>
+          <ThemeProvider>
+            <DensityProvider>
+              <AuthProvider>
+                <App />
+                <Toaster />
+              </AuthProvider>
+            </DensityProvider>
+          </ThemeProvider>
+        </SiteProvider>
       </I18nProvider>
     </QueryClientProvider>
   </React.StrictMode>,
