1c4f92f78603db39f45c2584553410d8e1aa6c40
Mr X <93528905+JulesZYTB@users.noreply.github.com>
2026-07-26 02:32:40 +0200

    feat(share-links): register public share-link controller

--- api/src/modules/proxies/proxies.module.ts
+++ api/src/modules/proxies/proxies.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PanelSubUserController } from './controllers/proxies.controller';
+import { ShareLinkPublicController } from './controllers/share-link-public.controller';
 
 /** CRUD des comptes proxy (UserProxy) côté panel admin. */
 @Module({
-  controllers: [PanelSubUserController],
+  controllers: [PanelSubUserController, ShareLinkPublicController],
 })
 export class ProxiesModule {}
