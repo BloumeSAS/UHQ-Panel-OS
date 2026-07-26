888172a7647a70269703fe5aaf6c5e8c37c02434
Mr X <93528905+JulesZYTB@users.noreply.github.com>
2026-07-26 03:21:59 +0200

    feat(subusers): register sub-user templates controller

--- api/src/modules/proxies/proxies.module.ts
+++ api/src/modules/proxies/proxies.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PanelSubUserController } from './controllers/proxies.controller';
 import { ShareLinkPublicController } from './controllers/share-link-public.controller';
+import { SubUserTemplatesController } from './controllers/subuser-templates.controller';
 
 /** CRUD des comptes proxy (UserProxy) côté panel admin. */
 @Module({
-  controllers: [PanelSubUserController, ShareLinkPublicController],
+  controllers: [PanelSubUserController, ShareLinkPublicController, SubUserTemplatesController],
 })
 export class ProxiesModule {}
